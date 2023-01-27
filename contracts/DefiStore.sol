b2223b3da0f04e6533be4a42da21a8c0dddadfc5
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-01-27 15:58:28 -0800

    added listing function in smart contract and basic test for list function

--- contracts/DefiStore.sol
+++ contracts/DefiStore.sol
@@ -5,6 +5,40 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract DefiStore {
-    string public name = "DefiStore";
-    
+    address public owner;
+
+    struct Item {
+        uint id;
+        string name;
+        string category;
+        string image;
+        uint cost;
+        uint rating;
+        uint stock;
+    }
+
+    mapping(uint => Item) public items;
+
+    constructor() { 
+        owner = msg.sender;
+    }
+
+    //list products
+    function list(uint _id, 
+    string memory _name, 
+    string memory _category,
+    string memory _image,
+    uint _cost,
+    uint _rating,
+    uint _stock
+    ) public {
+        //create Item sturct
+        Item memory item = Item(_id, _name, _category, _image, _cost, _rating, _stock);
+        //save Item struct
+        items[_id] = item;
+    }
+
+    //buy products
+
+    //withdraw funds
 }
--- test/DefiStore.js
+++ test/DefiStore.js
@@ -6,10 +6,33 @@ const tokens = (n) => {
 };
 
 describe("DefiStore", () => {
-  it("Has name", async () => {
+  let defiStore;
+
+  beforeEach(async () => {
+    //setup test accounts
+    [deployer, buyer] = await ethers.getSigners();
+    //deploys contract
     const DefiStore = await ethers.getContractFactory("DefiStore");
     defiStore = await DefiStore.deploy();
-    expect(await defiStore.name()).to.equal("DefiStore");
-    console.log(defiStore.name);
+  });
+
+  describe("Deployment", () => {
+    it("Check owner", async () => {
+      expect(await defiStore.owner()).to.equal(deployer.address);
+    });
+  });
+
+  describe("Listing", () => {
+    let transaction;
+    beforeEach(async () => {
+      transaction = await defiStore
+        .connect(deployer)
+        .list(1, "Shoes", "Footwear", "Image", 1, 5, 10);
+      await transaction.wait();
+    });
+    it("Returns item attributes", async () => {
+      const item = await defiStore.items(1);
+      expect(item.id).to.equal(1);
+    });
   });
 });
