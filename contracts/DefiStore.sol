60d71bbda0e1cd2b257ff035b760e936f18f94e8
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-01-27 18:05:06 -0800

    added buy function to smart contract with tests

--- contracts/DefiStore.sol
+++ contracts/DefiStore.sol
@@ -17,9 +17,17 @@ contract DefiStore {
         uint stock;
     }
 
+    struct Order {
+        uint time;
+        Item item;
+    }
+
     event List(string name, uint cost, uint quantity);
+    event Buy(address buyer, uint orderId, uint itemId);
 
     mapping(uint => Item) public items;
+    mapping(address => uint) public orderCount;
+    mapping(address => mapping(uint => Order)) public orders;
 
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -30,7 +38,7 @@ contract DefiStore {
         owner = msg.sender;
     }
 
-    //list products
+    //List products
     function list(uint _id, 
     string memory _name, 
     string memory _category,
@@ -40,15 +48,30 @@ contract DefiStore {
     uint _stock
     ) public onlyOwner {
         
-        //create Item sturct
+        //Create Item sturct
         Item memory item = Item(_id, _name, _category, _image, _cost, _rating, _stock);
-        //save Item struct
+        //Save Item struct
         items[_id] = item;
         //Triger event
         emit List(_name, _cost, _stock);
     }
 
-    //buy products
+    //Buy products
+    function buy(uint _id) public payable {
+        // Fetch item
+        Item memory item = items[_id];
 
-    //withdraw funds
+        require(msg.value >= item.cost);
+        require(item.stock > 0);
+        // Create order number
+        Order memory order = Order(block.timestamp, item);
+        // Add order for user
+        orderCount[msg.sender]++;
+        orders[msg.sender][orderCount[msg.sender]] = order;
+        //Subtract from stock
+        items[_id].stock = item.stock - 1;
+        // Triger event
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
+    }
+    //Withdraw funds
 }
--- test/DefiStore.js
+++ test/DefiStore.js
@@ -52,9 +52,39 @@ describe("DefiStore", () => {
       expect(item.rating).to.equal(RATING);
       expect(item.stock).to.equal(STOCK);
     });
-    it("Trigers List event"),
-      async () => {
-        expect(transaction).to.emit(defiStore, "List");
-      };
+    it("Emits List event", async () => {
+      expect(transaction).to.emit(defiStore, "List");
+    });
+  });
+  describe("Buying", () => {
+    let transaction;
+
+    beforeEach(async () => {
+      //List item
+      transaction = await defiStore
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+      //Buy item
+      transaction = await defiStore.connect(buyer).buy(ID, { value: COST });
+      await transaction.wait();
+    });
+
+    it("Updates buyer order count", async () => {
+      const result = await defiStore.orderCount(buyer.address);
+      expect(result).to.equal(1);
+    });
+    it("Adds the order", async () => {
+      const order = await defiStore.orders(buyer.address, 1);
+      expect(order.time).to.be.greaterThan(0);
+      expect(order.item.name).to.be.equal(NAME);
+    });
+    it("Updates contract balance", async () => {
+      const result = await ethers.provider.getBalance(defiStore.address);
+      expect(result).to.equal(COST);
+    });
+    it("Emits Buy event", () => {
+      expect(transaction).to.emit(defiStore, "Buy");
+    });
   });
 });
